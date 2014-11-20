9d78767de02acd3877b2a4823f08c886cf6cc9e9
Thomas Endres <Thomas-Endres@gmx.de>
2014-11-20 20:15:07 +0100

    Integrated error messages when trying to build an image that cannot be built because of the configuration

--- IntelRealSenseStart/Code/RealSense/Component/Hands/HandsImageBuilder.cs
+++ IntelRealSenseStart/Code/RealSense/Component/Hands/HandsImageBuilder.cs
@@ -2,20 +2,22 @@
 using IntelRealSenseStart.Code.RealSense.Config;
 using IntelRealSenseStart.Code.RealSense.Config.HandsImage;
 using IntelRealSenseStart.Code.RealSense.Data;
+using IntelRealSenseStart.Code.RealSense.Exception;
 using IntelRealSenseStart.Code.RealSense.Factory;
 
 namespace IntelRealSenseStart.Code.RealSense.Component.Hands
 {
     public class HandsImageBuilder
     {
+        private readonly HandsData handsData;
         private readonly HandsImageConfiguration.Builder handsImageConfigurationBuilder;
         private readonly HandsImageCreator.Builder handsImageCreator;
 
-        private readonly Configuration realSenseConfiguration;
-        private readonly HandsData handsData;
         private readonly ImageData imageData;
+        private readonly Configuration realSenseConfiguration;
 
-        private HandsImageBuilder(RealSenseFactory factory, Configuration realSenseConfiguration, HandsData handsData, ImageData imageData)
+        private HandsImageBuilder(RealSenseFactory factory, Configuration realSenseConfiguration, HandsData handsData,
+            ImageData imageData)
         {
             handsImageConfigurationBuilder = factory.Events.HandsImageConfiguration();
             handsImageCreator = factory.Components.HandsImageCreator();
@@ -27,24 +29,43 @@ namespace IntelRealSenseStart.Code.RealSense.Component.Hands
 
         public HandsImageBuilder WithBackgroundImage(HandsImageBackground backgroundImage)
         {
+            if (backgroundImage == HandsImageBackground.ColorImage && !realSenseConfiguration.ColorImageEnabled)
+            {
+                throw new RealSenseException("Cannot use background image since it is not configured");
+            }
+
             handsImageConfigurationBuilder.WithBackgroundImage(backgroundImage);
             return this;
         }
 
         public HandsImageBuilder WithOverlay(HandsImageOverlay overlay)
         {
+            if (overlay == HandsImageOverlay.HandsSegmentationImage &&
+                (!realSenseConfiguration.HandsDetectionEnabled ||
+                 !realSenseConfiguration.HandsDetection.SegmentationImageEnabled))
+            {
+                throw new RealSenseException("Cannot use hand segmentation image since it is not configured");
+            }
+            if (overlay == HandsImageOverlay.HandJoints && !realSenseConfiguration.HandsDetectionEnabled)
+            {
+                throw new RealSenseException("Cannot use hand joints since hands detection is not configured");
+            }
+
             handsImageConfigurationBuilder.WithOverlay(overlay);
             return this;
         }
 
         public Bitmap Create()
         {
-            return handsImageCreator.Build(handsData, imageData, realSenseConfiguration, handsImageConfigurationBuilder.Build()).Create();
+            return
+                handsImageCreator.Build(handsData, imageData, realSenseConfiguration,
+                    handsImageConfigurationBuilder.Build()).Create();
         }
 
         public class Builder
         {
-            public HandsImageBuilder Build(RealSenseFactory factory, Configuration realSenseConfiguration, HandsData handsData, ImageData imageData)
+            public HandsImageBuilder Build(RealSenseFactory factory, Configuration realSenseConfiguration,
+                HandsData handsData, ImageData imageData)
             {
                 return new HandsImageBuilder(factory, realSenseConfiguration, handsData, imageData);
             }
