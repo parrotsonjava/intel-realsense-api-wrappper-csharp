dba6eeecfa3666a39f7542b9d5ec3a622ccf8f79
Thomas Endres <Thomas-Endres@gmx.de>
2015-05-22 19:27:54 +0200

    First steps towards dialog workflow

---
+++ IntelRealSenseStart/Code/DialogFlow/Decision/DialogDecision.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelRealSenseStart.Code.DialogFlow.Decision
+{
+    public class DialogDecision<STATE>
+    {
+        private readonly STATE followUpState;
+        private readonly MatchesPredicate predicate;
+        private readonly Action<String> action;
+
+        public delegate bool MatchesPredicate(STATE currentState, String recognizedSentence);
+
+        public DialogDecision(STATE followUpState, MatchesPredicate predicate, Action<String> action)
+        {
+            this.followUpState = followUpState;
+            this.predicate = predicate;
+            this.action = action;
+        }
+
+        public bool matches(STATE currentState, String recognizedSentence)
+        {
+            return predicate.Invoke(currentState, recognizedSentence);
+        }
+
+        public STATE FollowUpState
+        {
+            get { return followUpState; }
+        }
+
+        public Action<String> Action
+        {
+            get { return action; }
+        }
+    }
+}
